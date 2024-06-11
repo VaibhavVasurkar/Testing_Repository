8fdc55ba896eefe5f2fc449bd10e1a700028e3f3
avinash-l <avinashlohar763@gmail.com>
2024-06-11 15:42:56 +0530

    Version3

--- file1.js
+++ file1.js
@@ -8,3 +8,9 @@ Thanks
 GC is also provided by Git.
 GC will work for you .No need to care about memory.
 Focus on leet code!
+There are total 4 objects inside Git.
+1.Commit
+2.Tree
+3.Blob
+4.Tags
+check it out!
